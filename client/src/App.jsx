1830d556e919ad2ef3bdaa56d1f2a0b103743268
eshwar210607 <eashwar210607@gmail.com>
2026-06-17 17:37:49 +0530

    register custom protected router links for online game room matchmaking environments

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,12 +6,16 @@ import Signup from './pages/Signup';
 import Home from './pages/Home';
 import ComputerMenu from './pages/ComputerMenu';
 import MultiplayerMenu from './pages/MultiplayerMenu';
+import OnlineDuel from './pages/games/OnlineDuel';
 import HectocSolo from './pages/games/HectocSolo';
 import Calculator from './pages/games/Calculator';
 import Sliding from './pages/games/Sliding';
 import HectocMulti from './pages/games/HectocMulti';
 import CalculatorMulti from './pages/games/CalculatorMulti';
 import SlidingMulti from './pages/games/SlidingMulti';
+
+// 🌟 IMPORT YOUR NEW LIVE DUELING MULTIPLAYER LOBBY HUB
+import OnlineLobby from './pages/OnlineLobby';
 
 
 function App() {
@@ -31,6 +35,10 @@ function App() {
           <Route path="/game/hectoc-multi" element={<ProtectedRoute><HectocMulti /></ProtectedRoute>} />
           <Route path="/game/calculator-multi" element={<ProtectedRoute><CalculatorMulti /></ProtectedRoute>} />
           <Route path="/game/sliding-multi" element={<ProtectedRoute><SlidingMulti /></ProtectedRoute>} />
+          
+          {/* 🌟 NEW ROUTE ENTRY: MOUNTS YOUR DYNAMIC WEB SOCKET MATCHMAKING CONTROLLER */}
+          <Route path="/game/online-lobby" element={<ProtectedRoute><OnlineLobby /></ProtectedRoute>} />
+          <Route path="/game/online-duel/:roomId" element={<ProtectedRoute><OnlineDuel /></ProtectedRoute>} />
         </Routes>
       </div>
     </Router>
